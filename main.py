5a8c9dfc698439e0e73efbec6cc325ccca23eea8
nastasija-m <78686458+nastasija-m@users.noreply.github.com>
2022-10-25 16:00:54 +0200

    cao

---
+++ main.py
@@ -0,0 +1,14 @@
+from urllib.request import Request, urlopen
+
+def fetch_random_dad_joke() -> str:
+    req = Request(
+        url="https://icanhazdadjoke.com/",
+        headers={
+            "Accept": "text/plain",
+            "User-Agent": "Workshop"
+        }
+    )
+    return urlopen(req).read().decode("utf-8")
+print("hi")
+
+print(fetch_random_dad_joke())
